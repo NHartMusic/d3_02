512a0e211bbf1ce7704b830b4b655ef0d320dfd9
Nick Hart <nhartmusic@gmail.com>
2020-06-07 17:18:08 +0200

    appending virtual dom elements from enter selection to the dom

--- data.js
+++ data.js
@@ -1,13 +1,23 @@
 const data = [
-    { width: 200, height: 100, fill: 'purple' }
+    { width: 200, height: 100, fill: 'purple' },
+    { width: 100, height: 60, fill: 'pink' },
+    { width: 50, height: 30, fill: 'red' }
 ]
 
 const svg = d3.select('svg')
 
-const rect = svg.select('rect')
+//join data to rects
+const rects = svg.selectAll('rect')
     .data(data)
-    .attr('width', (d, i, n) => d.width)
+
+//add attributes to rects already in dom
+rects.attr('width', (d, i, n) => d.width)
     .attr('height', d => d.height)
     .attr('fill', d => d.fill)
 
-console.log(rect)
+//append enter selection to dom
+rects.enter()
+    .append('rect')
+    .attr('width', (d, i, n) => d.width)
+    .attr('height', d => d.height)
+    .attr('fill', d => d.fill)
