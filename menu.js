be683a403a40b5fac6cc6026256d6e1444a6da00
Nick Hart <nhartmusic@gmail.com>
2020-06-08 13:46:08 +0200

    added scales, now i just need to invert them

--- menu.js
+++ menu.js
@@ -1,10 +1,29 @@
-const svg = d3.select('svg')
+const svg = d3
+    .select('.canvas')
+    .append('svg')
+    .attr('width', 600)
+    .attr('height', 600)
+
+//margin + dimensions
+const margin = { top: 20, right: 20, bottom: 100, left: 100 }
+const graphWidth = 600 - margin.left - margin.right
+const graphHeight = 600 - margin.top - margin.bottom
+
+const graph = svg
+    .append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+const xAxisGroup = graph.append('g')
+    .attr('transform', `translate(0, ${graphHeight})`)
+const yAxisGroup = graph.append('g')
 
 d3.json('menu.json').then(data => {
     const y = d3
         .scaleLinear()
-        .domain([0, 1000])
-        .range([0, 500])
+        .domain([0, d3.max(data, d => d.orders)])
+        .range([0, graphHeight])
 
     const x = d3
         .scaleBand()
@@ -13,10 +32,8 @@ d3.json('menu.json').then(data => {
         .paddingInner(0.2)
         .paddingOuter(0.2)
 
-    console.log(x('Bison Burger'))
-
     //join data to rects
-    const rects = svg
+    const rects = graph
         .selectAll('rect')
         .data(data)
 
@@ -33,4 +50,10 @@ d3.json('menu.json').then(data => {
         .attr('height', d => y(d.orders))
         .attr('fill', 'orange')
         .attr('x', d => x(d.name))
+
+    const xAxis = d3.axisBottom(x)
+    const yAxis = d3.axisLeft(y)
+
+    xAxisGroup.call(xAxis)
+    yAxisGroup.call(yAxis)
 })
