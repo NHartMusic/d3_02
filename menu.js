f59bccbebb3465fb98aa57ad7d07b3515a16ba7a
Nick Hart <nhartmusic@gmail.com>
2020-06-14 16:02:39 +0200

    successfully updating graph when firestore is updated

--- menu.js
+++ menu.js
@@ -81,13 +81,27 @@ const update = (data) => {
 
 }
 
-db.collection('dishes').get().then(res => {
+let data = []
 
-    let data = []
-    res.docs.forEach(doc => {
-        data.push(doc.data())
+db.collection('dishes').onSnapshot(res => {
+
+    res.docChanges().forEach(change => {
+        const doc = { ...change.doc.data(), id: change.doc.id }
+        switch (change.type) {
+            case 'added':
+                data.push(doc)
+                break
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id)
+                data[index] = doc
+                break
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id)
+                break
+            default:
+                break
+        }
     })
 
     update(data)
-
 })
